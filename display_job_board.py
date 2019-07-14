5c2e0ced4416f9848e1b4f224b5a14b14096fdd5
Sujeet Pillai <sujit97@gmail.com>
2019-07-14 01:27:41 -0400

    Replacing QTableView with QTableWidget
    #   Add QTableWidget using Designer Tool - DONE
    #   Copy code for Widget and replace TableView - DONE
    #   Populate values in Widget and test run - DONE
    
    #   Edit and add all other functions - DONE

--- display_job_board.py
+++ display_job_board.py
@@ -11,115 +11,8 @@ class JobStatus(Enum):
     
 @unique
 class JobStatusColors(Enum):
-    UNOPENED = 'black'
-    OPENED = 'blue'
-
-#   Add QTableWidget using Designer Tool - DONE
-#   Copy code for Widget and replace TableView - DONE
-#   Populate values in Widget and test run - DONE
-
-#   Edit and add all other functions
-
-#class PandasModel(QtCore.QAbstractTableModel): 
-#    def __init__(self, df = pd.DataFrame(), parent=None): 
-#        QtCore.QAbstractTableModel.__init__(self, parent=parent)
-#        self._df = df
-#
-#    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
-#        if role != QtCore.Qt.DisplayRole:
-#            return QtCore.QVariant()
-#
-#        if orientation == QtCore.Qt.Horizontal:
-#            try:
-#                return self._df.columns.tolist()[section]
-#            except (IndexError, ):
-#                return QtCore.QVariant()
-#        elif orientation == QtCore.Qt.Vertical:
-#            try:
-#                # return self.df.index.tolist()
-#                return self._df.index.tolist()[section]
-#            except (IndexError, ):
-#                return QtCore.QVariant()
-#
-#    def data(self, index, role):
-#        if role not in [QtCore.Qt.DisplayRole, QtCore.Qt.BackgroundColorRole]:
-#            return QtCore.QVariant()
-#
-#        if role == QtCore.Qt.BackgroundColorRole:
-##            if index.column() == 5:
-##                return QtGui.QColor(204,120,200)
-#            if index.column() == 5 and str(index.data()) == JobStatus.UNOPENED.value:
-#                return QtGui.QColor(204,120,200)
-#                
-##                if index.row() == JobStatus.OPENED.value:
-###                    return QtGui.QBrush(QtCore.Qt.green)
-##                    return QtCore.Qt.green
-##                elif index.row() == JobStatus.UNOPENED.value:
-###                    return QtGui.QBrush(QtCore.Qt.red)
-##                    return QtCore.Qt.red
-#            
-#        if not index.isValid():
-#            return QtCore.QVariant()
-#
-#        return QtCore.QVariant(str(self._df.ix[index.row(), index.column()]))
-#    
-#    def get_data(self, row, column, value_type = 'QVariant'):
-#        if value_type == 'QVariant' :
-#            return QtCore.QVariant(str(self._df.ix[row, column]))
-#        elif value_type == 'str':
-#            return str(self._df.ix[row, column])
-#        
-#    
-#    
-#    def setData(self, index, value, role):
-#        row = self._df.index[index.row()]
-#        col = self._df.columns[index.column()]
-#        if hasattr(value, 'toPyObject'):
-#            # PyQt4 gets a QVariant
-#            value = value.toPyObject()
-#        else:
-#            # PySide gets an unicode
-#            dtype = self._df[col].dtype
-#            if dtype != object:
-#                value = None if value == '' else dtype.type(value)
-#        self._df.set_value(row, col, value)
-#        return True
-#    
-#           
-#        
-#    
-#    def set_data(self, row, column, value):
-#        row = self._df.index[row]
-#        col = self._df.columns[column]
-#        if hasattr(value, 'toPyObject'):
-#            # PyQt4 gets a QVariant
-#            value = value.toPyObject()
-#        else:
-#            # PySide gets an unicode
-#            dtype = self._df[col].dtype
-#            if dtype != object:
-#                value = None if value == '' else dtype.type(value)
-#        self._df.set_value(row, col, value)
-#        
-#        start_of_row = self.index(row, 0)
-#        end_of_row = self.index(row, self.columnCount())
-#        self.dataChanged.emit(start_of_row, end_of_row)
-#        return True
-#
-#    def rowCount(self, parent=QtCore.QModelIndex()): 
-#        return len(self._df.index)
-#
-#    def columnCount(self, parent=QtCore.QModelIndex()): 
-#        return len(self._df.columns)
-#
-#    def sort(self, column, order):
-#        colname = self._df.columns.tolist()[column]
-#        self.layoutAboutToBeChanged.emit()
-#        self._df.sort_values(colname, ascending= order == QtCore.Qt.AscendingOrder, inplace=True)
-#        self._df.reset_index(inplace=True, drop=True)
-#        self.layoutChanged.emit()
-#    
-
+    UNOPENED = '#ffffcc'
+    OPENED = '#ff9478'
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -142,11 +35,7 @@ class Ui_MainWindow(object):
         self.job_table_widget.setRowCount(0)
         self.job_table_widget.setSortingEnabled(True)
         
-#        self.job_table_view = QtWidgets.QTableView(self.centralwidget)
-#        self.job_table_view.setGeometry(QtCore.QRect(10, 0, 781, 531))
-#        self.job_table_view.setSortingEnabled(True)
-#        self.job_table_view.setObjectName("job_table_view")
-        
+   
         self.scrape_all_pages = QtWidgets.QCheckBox(self.centralwidget)
         self.scrape_all_pages.setGeometry(QtCore.QRect(10, 540, 121, 17))
         self.scrape_all_pages.setObjectName("scrape_all_pages")
@@ -183,27 +72,27 @@ class Ui_MainWindow(object):
         self.scrape_all_pages.toggled.connect(self.toggle_page_field)
         
         self.num_pages_to_scrape.sliderMoved.connect(self.update_page_desc)
+        self.job_table_widget.itemDoubleClicked.connect(self.open_job_link)
         
-#        self.job_table_view.setSelectionMode(1)
-#        self.job_table_view.setSelectionBehavior(1)
-##        self.job_table_view.horizontalHeader().setStretchLastSection(True)
-#        
-#        self.job_table_view.doubleClicked.connect(self.open_job_link) 
-#        self.job_table_view.setItemDelegate
-        
-#    def open_job_link(self, model_index):
-#        print("\nTable Double Clicked\n")
-#        
-#        print(model_index.row())
-#        print(model_index.column())
-#        print(model_index.data())
-#        
-#        if model_index.column() == 2:
-##            QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://www.indeed.com" + model_index.data()))
-#            self.job_table_view.model().set_data(model_index.row(), 5, JobStatus.OPENED.value)
-            
-            
-
+    
+    def color_row(self, row_index, color = QtGui.QColor):
+        for col_index in range(self.job_table_widget.columnCount()):
+            self.job_table_widget.item(row_index, col_index).setBackground(color)
+    
+    
+    def refresh_status_colors(self):
+        for i in range(self.job_table_widget.rowCount()):
+            self.color_row(i, QtGui.QColor(JobStatusColors[JobStatus(self.job_table_widget.item(i,5).text()).name].value))
+    
+    
+    def open_job_link(self, item):
+        print("\nTable Double Clicked\n")
+        if item.column() == 2:
+            QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://www.indeed.com" + item.text()))
+            self.job_table_widget.setItem(item.row(), 5, QtWidgets.QTableWidgetItem(JobStatus.OPENED.value))
+            self.color_row(item.row(), QtGui.QColor(JobStatusColors.OPENED.value))
+   
+    
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Job Board"))
@@ -211,7 +100,7 @@ class Ui_MainWindow(object):
         self.savecancel_board_btn.setText(_translate("MainWindow", "Save and Cancel"))
         self.scrape_all_pages.setText(_translate("MainWindow", "Scrape all pages"))
         self.page_desc_label.setText(_translate("MainWindow", "Scrape all pages"))
-
+    
     
     def refresh_job_board(self):
         self.refresh_board_btn.setEnabled(False)
@@ -237,10 +126,9 @@ class Ui_MainWindow(object):
         for row_index in range(row_count):
             for col_index in range(col_count):
                 self.job_table_widget.setItem(row_index, col_index, QtWidgets.QTableWidgetItem(str(job_df.ix[row_index, col_index])))
-        
+        self.refresh_status_colors()
         self.refresh_board_btn.setEnabled(True)
-#        self.job_table_view.resizeColumnsToContents
-    
+
     
     def toggle_page_field(self):
         self.num_pages_to_scrape.setEnabled(not self.scrape_all_pages.isChecked())
@@ -253,6 +141,7 @@ class Ui_MainWindow(object):
         print("Moved")
         self.page_desc_label.setText("Scrape " + str(self.num_pages_to_scrape.value()) + " page/s")
     
+ 
     
 if __name__ == "__main__":
     
